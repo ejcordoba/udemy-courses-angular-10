71f9e0ee399bd0a77888402a206944bc1b75bbc5
eduardo <eduardo@debian>
2020-08-07 13:53:07 +0200

    section 2 lesson 17 update

--- demo-typescript/app.js
+++ demo-typescript/app.js
@@ -1,7 +1,12 @@
 "use strict";
 //Función auto-invaocada para evitar errores de variables reutilizadas
 (function () {
+    // Si declaramos la función en una constante no podremos sobreescribir la declaración de la función
     var miFuncion = function (a) {
-        return a;
+        return a.toUpperCase();
     };
+    //La misma función en versión "Función de flecha", si sólamente se va a retornar una línea de código se pueden ahorrar las llaves y el return
+    var miFuncionF = function (a) { return a.toUpperCase(); };
+    console.log(miFuncion('Normal'));
+    console.log(miFuncionF('Flecha'));
 })();
--- demo-typescript/app.ts
+++ demo-typescript/app.ts
@@ -1,11 +1,15 @@
 //Función auto-invaocada para evitar errores de variables reutilizadas
 (function(){
 
-    let miFuncion = function( a: string) {
-        return a;
+    // Si declaramos la función en una constante no podremos sobreescribir la declaración de la función
+    const miFuncion = function( a: string) {
+        return a.toUpperCase();
     }
+    //La misma función en versión "Función de flecha", si sólamente se va a retornar una línea de código se pueden ahorrar las llaves y el return
+    const miFuncionF = ( a: string ) =>  a.toUpperCase();
 
-
+    console.log( miFuncion('Normal') );
+    console.log( miFuncionF('Flecha') );
 
 
 
