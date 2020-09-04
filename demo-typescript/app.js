74492b24aeb3b291afa1e96e82cc0a1a570a6fba
eduardo <eduardo@debian>
2020-09-04 13:59:47 +0200

    section 2 lesson 24 to be done

--- demo-typescript/app.js
+++ demo-typescript/app.js
@@ -3,11 +3,12 @@
 (() => {
     //Convención de nombres de clases, capitalizar palabras
     class Avenger {
-        constructor() {
-            //Nombre: tipo
-            this.nombre = "Sin nombre";
+        //Constructor: función que se ejecuta cuando se crea una nueva instancia de una clase
+        constructor(nombre, equipo) {
+            this.nombre = nombre; //this apunta a la propiedad nombre de la clase Avenger, y será igual al valor que se le pasa al constructor
+            this.equipo = equipo;
         }
     }
-    const antman = new Avenger();
+    const antman = new Avenger('Antman', 'Capi');
     console.log(antman);
 })();
--- demo-typescript/app.ts
+++ demo-typescript/app.ts
@@ -4,16 +4,23 @@
     //Convención de nombres de clases, capitalizar palabras
     class Avenger {
         //Nombre: tipo
-        nombre: string = "Sin nombre";
+        nombre: string;
         equipo: string;
         nombreReal: string; //lowercamelcase
 
         puedePelear: boolean;
         peleasGanadas: number;
 
+        //Constructor: función que se ejecuta cuando se crea una nueva instancia de una clase
+        constructor( nombre: string , equipo: string){
+            this.nombre = nombre; //this apunta a la propiedad nombre de la clase Avenger, y será igual al valor que se le pasa al constructor
+            this.equipo = equipo;
+
+        }
+
     }
 
-    const antman = new Avenger();
+    const antman = new Avenger('Antman','Capi');
 
     console.log(antman);
 })();
