b08da7d9f9a2d4fd925721ebf63e1a1f17e23f4c
Eduardo Cordoba <eduardo.cordoba.j@gmail.com>
2020-09-07 17:21:12 +0200

    Added exam ts file

--- demo-typescript/app.ts
+++ demo-typescript/app.ts
@@ -1,5 +1,47 @@
-//Función auto-invocada de flecha para evitar errores de variables reutilizadas
 (()=>{
 
- 
+  // Uso de Let y Const
+  var nombre = 'Ricardo Tapia';
+  var edad = 23;
+
+  var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+  };
+
+
+  // Cree una interfaz que sirva para validar el siguiente objeto
+  var batman = {
+    nombre: 'Bruno Díaz',
+    artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
+  }
+
+  // Convertir esta funcion a una funcion de flecha
+  function resultadoDoble( a, b ){
+    return (a + b) * 2
+  }
+
+  // Función con parametros obligatorios, opcionales y por defecto
+  // donde NOMBRE = obligatorio
+  //       PODER  = opcional
+  //       ARMA   = por defecto = 'arco'
+  function getAvenger( nombre, poder, arma ){
+    var mensaje;
+    if( poder ){
+      mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
+    }else{
+      mensaje = nombre + ' tiene un ' + poder
+    }
+  };
+
+  // Cree una clase que permita manejar la siguiente estructura
+  // La clase se debe de llamar rectangulo,
+  // debe de tener dos propiedades:
+  //   * base
+  //   * altura
+  // También un método que calcule el área  =  base * altura,
+  // ese método debe de retornar un numero.
+
+
+
 })();
