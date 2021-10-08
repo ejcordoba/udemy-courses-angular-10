f3713cc4b37fced651292a79d80337b9a0cdb3bf
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2021-10-08 12:14:35 +0200

    swiper problem trying to downgrade

--- 11-peliculasApp/src/app/components/slideshow/slideshow.component.ts
+++ 11-peliculasApp/src/app/components/slideshow/slideshow.component.ts
@@ -1,16 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
-
+import { Swiper } from 'swiper';
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[];
 
   constructor() { }
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'vertical',
+      loop: true,
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  }
 
   ngOnInit(): void {
     console.log(this.movies);
